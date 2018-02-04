434407ab5e6e658e8d838eb88cb914bd578670d8
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-02-04 14:35:08 -0800

    code logic to place orders

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// Then create a Node application called bamazonCustomer.js.
 // Dependencies
 var mysql = require("mysql")
 var inquirer = require("inquirer")
@@ -14,11 +14,11 @@ var connection = mysql.createConnection({
 })
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-  if (err) throw err;
-  // console.log('connected as id ' + connection.threadID);
-  queryAllProducts();
-  // // run the start function after the connection is made to prompt the user
-  // start();
+  if (err) throw err
+  // Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+  queryAllProducts()
+  // The app should then prompt users with two messages.
+  start()
 })
 function queryAllProducts() {
 	connection.query('SELECT * FROM products', function(err, res){
@@ -29,13 +29,43 @@ function queryAllProducts() {
 		console.log('--------------------------------------')
 	})
 }
-// The app should then prompt users with two messages.
 
 // The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
+function start() {
+  inquirer
+    .prompt([
+      {
+        name: "item_id",
+        type: "input",
+        message: "What is the ID of the product you would like to buy?"
+      },
+      {
+        name: "units",
+        type: "input",
+        message: "How many units of this product would you like to buy?"
+      },
+    ])
 // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
 // However, if your store does have enough of the product, you should fulfill the customer's order.
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
+    .then(function(answer) {
+		connection.query('SELECT * FROM products WHERE item_id = '+answer.item_id, function(err, res){
+			if (res[0].stock_quantity>=answer.units) {
+				var updatedQty = res[0].stock_quantity-answer.units
+				connection.query('UPDATE products SET stock_quantity = '+updatedQty+' WHERE item_id = '+answer.item_id, function(err,res){
+				})
+				var total = res[0].price * answer.units
+				console.log('Your order was placed.  Your total is $'+total)
+				console.log("Let's shop 'til we drop!!")
+				start()
+			} else {
+				console.log("I'm sorry, we don't have quite enough " + res[0].product_name+" available.\nWould you like to purchase something else?")
+				start()
+			}
+		})
+    });
+}
 // If this activity took you between 8-10 hours, then you've put enough time into this assignment. Feel free to stop here -- unless you want to take on the next challenge.
