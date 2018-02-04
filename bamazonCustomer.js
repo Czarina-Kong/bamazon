f587c8bc34b71286b9b0bf229e26014e9d471f41
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-02-04 12:41:56 -0800

    code function to display data from products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,34 @@
 // Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
+// Dependencies
+var mysql = require("mysql")
+var inquirer = require("inquirer")
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "abc123",
+  database: "bamazon_db"
+})
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // console.log('connected as id ' + connection.threadID);
+  queryAllProducts();
+  // // run the start function after the connection is made to prompt the user
+  // start();
+})
+function queryAllProducts() {
+	connection.query('SELECT * FROM products', function(err, res){
+		console.log("***WELCOME TO CZARINA'S BAMAZON APP***\n*Take a look at what I have for sale!*")
+		for (var i = 0; i < res.length; i++) {
+			console.log(res[i].item_id+' | '+res[i].product_name+' | $'+res[i].price)
+		}
+		console.log('--------------------------------------')
+	})
+}
 // The app should then prompt users with two messages.
 
 // The first should ask them the ID of the product they would like to buy.
